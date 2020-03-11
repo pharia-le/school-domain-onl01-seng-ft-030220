65064c2c7dfc59a139aa33e0f59e2de13c84acd5
Pharia Le <pharia.le@gmail.com>
2020-03-11 21:48:44 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,33 @@
-# code here!
+class School
+  
+  attr_accessor :roster, :grade
+  attr_writer :name
+  
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  
+  def add_student(student, grade)
+    @student = {grade => student}
+    @student.each do |grade, name|
+      if @roster.include?(grade)
+        roster[grade] << name
+      else  
+        @roster[grade] = []          
+        @roster[grade] << name
+      end
+    end
+  end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+  
+  def sort 
+    @roster.each do |grade, students|
+      students.sort!
+    end
+  end
+  
+end
